c533ca115a5b96a00f93e0bc4ff180d4f585ff8d
Meghan Hein <meghan.hein@gmail.com>
2020-10-14 15:30:13 -0700

    netlify form handling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,6 @@ import "./Footer.scss"
 
 export default function Footer(props) {
   const [showModal, setShowModal] = useState(false)
-  const [user, setUser] = useState({
-    emailValue: '',
-    nameValue: '',
-  })
 
   function openModal() {
     setShowModal(true)
@@ -27,44 +23,37 @@ export default function Footer(props) {
       <iframe title="spotify" src="https://open.spotify.com/embed/album/3aBvYsTSyCAzGyp1buNzXz" width="250" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       </span>
       { props.subscribe === 'in-footer' &&
-        <form name="footer-subscribe" action="https://cayleythomas.us20.list-manage.com/subscribe/post" method="POST" noValidate id="footer-form" data-netlify="true">
-        <input type="hidden" name="u" value="78686e4b82246c5bba3a92bd0"/>
-        <input type="hidden" name="id" value="fef6220761"/>
-        <label htmlFor='MERGE1'>
-            <input 
-                type="text" 
-                name="FNAME" 
-                id="MERGE1" 
-                placeholder="Name"
-                value={user.nameValue} 
-                onChange={(e)=>{setUser({nameValue: e.target.value});}}
-            />
-        </label>
-        <label htmlFor='MERGE0'>
-            <input 
-                type="email" 
-                name="EMAIL" 
-                id="MERGE0"
-                placeholder="Email"
-                value={user.emailValue} 
-                onChange={ (e)=>{setUser({emailValue: e.target.value});} } 
-                autoCapitalize="off" 
-                autoCorrect="off"
-              /> 
-        </label>
-        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="footer-form-button"/>
-  
-        <div style={{position: 'absolute', left: '-5000px'}} aria-hidden='true' aria-label="Please leave the following three fields empty">
-            <label htmlFor="b_name">Name: </label>
-            <input type="text" name="b_name" tabIndex="-1" defaultValue="" placeholder="Freddie" id="b_name"/>
-  
-            <label htmlFor="b_email">Email: </label>
-            <input type="email" name="b_email" tabIndex="-1" defaultValue="" placeholder="youremail@gmail.com" id="b_email"/>
-  
-            <label htmlFor="b_comment">Comment: </label>
-            <textarea name="b_comment" tabIndex="-1" placeholder="Please comment" id="b_comment"></textarea>
-        </div>
-      </form>
+        <Mailchimp
+        className='footer-form'
+        data-netlify='true'
+        data-netlify-honeypot="bot-field"
+        method='post'
+        action='https://cayleythomas.us20.list-manage.com/subscribe/post?u=78686e4b82246c5bba3a92bd0&amp;id=fef6220761'
+        fields={[
+          {
+            name: 'FNAME',
+            placeholder: 'Name',
+            type: 'text',
+            required: true
+          },
+          {
+            name: 'EMAIL',
+            placeholder: 'Email',
+            type: 'email',
+            required: true
+          }
+        ]}
+        messages = {
+          {
+            sending: "Sending...",
+            success: "Thank you for subscribing!",
+            error: "An unexpected internal error has occurred.",
+            empty: "You must write an e-mail.",
+            duplicate: "Too many subscribe attempts for this email address",
+            button: "Subscribe"
+          }
+        }
+        />
       }
       <div id="right-footer">
         <ul id="social">
