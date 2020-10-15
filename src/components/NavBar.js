e4a3954ffe94bd7cf35b22c448ad47de1727b0ed
Meghan Hein <meghan.hein@gmail.com>
2020-10-14 20:50:47 -0700

    add nav bar icon for responsive design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import './NavBar.scss'
-
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import './NavBar.scss';
 
 export default function NavBar(props) {
+  const [menu, setMenu] = useState(false)
 
   function clickHandler(e){
     props.setPage(e.currentTarget.dataset.id)
   }
 
+  function openMenu() {
+    menu ? setMenu(false) : setMenu(true)
+  }
+
   return (
-    <div>
+    <div id="nav-menu">
       <ul id={props.id}>
         <li onClick={clickHandler} data-id='home'>HOME</li>
         <li onClick={clickHandler} data-id='about bar'>ABOUT</li>
@@ -18,6 +24,7 @@ export default function NavBar(props) {
         <li onClick={clickHandler} data-id='subscribe bar'>MAILING LIST</li>
         <li><a href="https://cayleythomas.bandcamp.com/merch" target="_blank" rel="noopener noreferrer">SHOP</a></li>
       </ul>
+      <FontAwesomeIcon id='menu-bar' icon={faBars} onClick={openMenu}/>
     </div>
   )
 }
