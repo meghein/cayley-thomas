227c63e62b1467c440f7f5e1017ec5cbd6a79b67
Meghan Hein <meghan.hein@gmail.com>
2020-10-14 21:43:47 -0700

    add nav menu for mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,10 +8,17 @@ export default function NavBar(props) {
 
   function clickHandler(e){
     props.setPage(e.currentTarget.dataset.id)
+    setMenu(false)
   }
 
   function openMenu() {
     menu ? setMenu(false) : setMenu(true)
+  }
+
+  window.onclick = function(event) {
+    if (event.target === document.getElementById("open-menu")) {
+      setMenu(false);
+    }
   }
 
   return (
@@ -25,6 +32,18 @@ export default function NavBar(props) {
         <li><a href="https://cayleythomas.bandcamp.com/merch" target="_blank" rel="noopener noreferrer">SHOP</a></li>
       </ul>
       <FontAwesomeIcon id='menu-bar' icon={faBars} onClick={openMenu}/>
+      {menu &&
+        <div id="open-menu">
+        <ul id="toggled-menu">
+          <li onClick={clickHandler} data-id='home'>HOME</li>
+          <li onClick={clickHandler} data-id='about bar'>ABOUT</li>
+          <li onClick={clickHandler} data-id='media bar'>MEDIA</li>
+          <li onClick={clickHandler} data-id='contact bar'>CONTACT</li>
+          <li onClick={clickHandler} data-id='subscribe bar'>MAILING LIST</li>
+          <li><a href="https://cayleythomas.bandcamp.com/merch" target="_blank" rel="noopener noreferrer">SHOP</a></li>
+        </ul>
+        </div>
+      }
     </div>
   )
 }
