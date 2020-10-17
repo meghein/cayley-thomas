bc234821f02bf13d9810ceb54d4f69267a40df8a
Meghan Hein <meghan.hein@gmail.com>
2020-10-16 17:11:58 -0700

    change bio and line spacing

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,13 @@ export default function About() {
     <div id='about'>
       <img src='images/in-flowers.jpg' alt='About in flowers'/>
       <p>
-        Cayley Thomas is a singer, guitarist and songwriter whose natural affinity for constructing catchy melodies reveals her striking vocal range and character. Under a delirious guise of simplicity, she weaves sensitive eccentricities and cathartic declarations into her own brand of unique, knowledgeable pop.<br />
-        <br />
-        Cayley’s most recent EP 'How Else Can I Tell You?' is a collection of songs that reflect upon a period of subconscious overwhelm and the subsequent shift into conscious thought and awareness. Recorded between Cayley’s hometown of Edmonton, AB and  Toronto, ON - the album deliberately combines Hi-Fi studio production and Lo-Fi living room charm. ‘How Else Can I Tell You?’ is fresh enough for modern indie rock ears to find familiar, with spells of synth and its modern production, all the while drawing on the fuzzy raw warmth of our favourite sounds from mid-century rock and soul. It features Cayley Thomas at the helm and is tied together by producers Nik Kozub (Shout Out Out Out Out, Humans, The Wet Secrets) and Steve Chahley (U.S. Girls, FRIGS, Badge Époque, Darlene Shrugg.) 
+      Edmonton born Cayley Thomas is a multidisciplinary singer, songwriter and performer whose natural affinity for constructing catchy melodies is matched by her striking vocal range and versatility. Classically trained in voice & theatre with a Bachelor of Fine Arts in Acting from the University of Alberta, Thomas weaves sensitive eccentricities and cathartic declarations into her own brand of unique, knowledgeable pop.
+      <br/>
+      <br/>
+      Recorded between Edmonton and Toronto, Cayley’s latest record, ‘How Else Can I Tell You?’ features Thomas at the production helm alongside co-producers Nik Kozub (Shout Out Out Out Out, Humans, The Wet Secrets) and Steve Chahley (U.S. Girls, FRIGS, Badge Époque.) Thomas was hailed by Paste Magazine as one of The Best New Canadian Artists You Need to Know in 2020, making their 25 Best EP’s of 2020 list. Thomas’ single “Two Minds” enjoyed 5 weeks on CBC’s National Top 20 and Exclaim! gave the EP a glowing 8/10 review, calling it, “...a flawless marriage of hi-fi production and lo-fi living-room pop…”, naming Cayley one of 9 Emerging Artists You Should Hear. 
+      <br/>
+      <br/>
+      Thomas has a natural ability of crafting songs that are fresh enough for modern indie-rock ears, while drawing on the fuzzy raw warmth of her favourite sounds from mid-century rock and soul. 
       </p>
     </div>
   )
