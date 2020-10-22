688b4c33ec6c092ee51dd2a8f25a5395b4c5d0cf
Meghan Hein <meghan.hein@gmail.com>
2020-10-21 17:40:04 -0700

    adjust contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,10 +7,10 @@ export default function Contact() {
       <div className='form'>
         <h2>Artist</h2>
         <h3>Cayley Thomas</h3>
-        <p>info@cayleythomas.com</p>
+        <a href="mailto:info@cayleythomas.com">info@cayleythomas.com</a>
         <h2>Management</h2>
         <h3>Ryan Gullen</h3>
-        <p>ryan@velvetantlermgmt.com</p>
+        <a href="mailto:ryan@velvetantlermgmt.com">ryan@velvetantlermgmt.com</a>
       </div>
     </div>
   )
