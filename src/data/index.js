b90944956ac4c18d32f59def024b0383018efb96
Meghan Hein <meghan.hein@gmail.com>
2021-09-08 15:50:09 -0700

    update bio

--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,7 @@ const about = {
 		alt: 'About in flowers',
 	},
 	bio: [
-		'Edmonton born Cayley Thomas is a multidisciplinary singer, songwriter and performer whose natural affinity for constructing catchy melodies is matched by her striking vocal range and versatility. Classically trained in voice & theatre with a Bachelor of Fine Arts in Acting from the University of Alberta, Thomas weaves sensitive eccentricities and cathartic declarations into her own brand of unique, knowledgeable pop.',
+		'Cayley Thomas is a multidisciplinary singer, songwriter and performer from Edmonton, Alberta whose natural affinity for constructing catchy melodies is matched by her striking vocal range and versatility. Classically trained in voice & theatre with a Bachelor of Fine Arts in Acting from the University of Alberta, Thomas weaves sensitive eccentricities and cathartic declarations into her own brand of unique, knowledgeable pop.',
 
 		'Recorded between Edmonton and Toronto, Cayley’s latest record, ‘How Else Can I Tell You?’ features Thomas at the production helm alongside co-producers Nik Kozub (Shout Out Out Out Out, Humans, The Wet Secrets) and Steve Chahley (U.S. Girls, FRIGS, Badge Époque.) Thomas was hailed by Paste Magazine as one of The Best New Canadian Artists You Need to Know in 2020, making their 25 Best EP’s of 2020 list. Thomas’ single “Two Minds” enjoyed 5 weeks on CBC’s National Top 20 and Exclaim! gave the EP a glowing 8/10 review, calling it, “...a flawless marriage of hi-fi production and lo-fi living-room pop…”, naming Cayley one of 9 Emerging Artists You Should Hear.',
 
