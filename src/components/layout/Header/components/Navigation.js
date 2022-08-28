4f7bb42218f96515ddb9dc605f4254909813110a
Meghan Hein <meghan.hein@gmail.com>
2022-08-28 20:34:06 +0800

    feat: add links to nav

--- src/components/layout/Header/components/Navigation.js
+++ src/components/layout/Header/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavBar } from './Navigation.styles'
 import { Box } from 'atomic-layout'
+import { Link } from 'react-router-dom'
 
 const navItems = ['home', 'about', 'media', 'subscribe']
 
@@ -8,10 +9,12 @@ export function Navigation (props) {
   return (
     <NavBar>
       {navItems.map((item) => {
-        const title = item === 'subscribe' ? 'MAILING LIST' : item.toUpperCase()
+        const title = item === 'subscribe'
+          ? 'MAILING LIST'
+          : item.toUpperCase()
         return (
           <Box key={item} as='li'>
-            {title}
+            <Link to={`/${item}`}>{title}</Link>
           </Box>
         )
       })}
